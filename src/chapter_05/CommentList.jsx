fedba14ab1b07dee46adbffcebfcdcf76c632474
jam759 <jemin6372@naver.com>
2025-08-11 18:21:10 +0900

    init

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+function CommentList(props){
+    return (
+        <div>
+            <Comment name={"이민제"} comment={"안녕하세요, 소플입니다. "}/>
+        </div>
+    );
+}
+
+export default CommentList;
